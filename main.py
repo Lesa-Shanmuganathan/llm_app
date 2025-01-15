5fdcebc23ccea89b197572aba18d2dcf888cbce1
Lesa-Shanmuganathan <lesa38835656@gmail.com>
2025-01-15 16:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ load_dotenv()
 API_KEY= os.environ['OPENAI_API_KEY']
 
 
-
 st.title("Prompt-driven analysis with PandasAI")
 uploaded_file=st.file_uploader("Upload a CSV file for analysis",type=['csv'])
 
 if uploaded_file is not None:
     df=pd.read_csv(uploaded_file)
     st.write(df.head(3))
+
+    prompt=st.text_area("Enter your prompt:")
