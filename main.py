f54b3c2a02a4e547ddbce57c4aad7781c1ee15e9
Lesa-Shanmuganathan <lesa38835656@gmail.com>
2025-01-15 16:07:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ API_KEY= os.environ['OPENAI_API_KEY']
 
 
 st.title("Prompt-driven analysis with PandasAI")
-st.file_uploader("Upload a CSV file for analysis",type=['csv'])
+uploaded_file=st.file_uploader("Upload a CSV file for analysis",type=['csv'])
+
+if uploaded_file is not None:
+    df=pd.read_csv(uploaded_file)
+    st.write(df.head(3))
