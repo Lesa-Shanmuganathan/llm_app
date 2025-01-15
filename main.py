aea5428fd79731b9deb2441ad97b7003b42435b9
Lesa-Shanmuganathan <lesa38835656@gmail.com>
2025-01-15 16:02:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from dotenv import load_dotenv
 import os 
 import streamlit as st
 
+
+
 load_dotenv()
-
-
-
 API_KEY= os.environ['OPENAI_API_KEY']
+
+
+
 st.title("Prompt-driven analysis with PandasAI")
+st.file_uploader("Upload a CSV file for analysis",type=['csv'])
